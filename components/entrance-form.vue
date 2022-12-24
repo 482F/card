5f571f36d6267c87ff58e7206e7d17d44d7fb9d3
482F <482F.git.mail@482f.net>
2022-12-25 00:00:00 +0900

    色関係の処理を追加

--- components/entrance-form.vue
+++ components/entrance-form.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    <a-text-field v-model="tempName" placeholder="プレイヤー名" />
-    <a-text-field v-model="tempPassword" placeholder="password" />
+  <div class="entrance-form">
+    <div class="inputs">
+      <a-text-field
+        class="name"
+        v-model="tempName"
+        placeholder="プレイヤー名"
+      />
+      <a-text-field
+        class="password"
+        v-model="tempPassword"
+        placeholder="password"
+      />
+      <v-color-picker class="color" v-model="tempColor" hide-inputs />
+    </div>
     <v-btn
       block
       depressed
@@ -10,6 +21,8 @@
         () => {
           $emit('update:name', tempName)
           $emit('update:password', tempPassword)
+          $emit('update:color', tempColor)
+          $emit('loggined')
         }
       "
     >
@@ -34,12 +47,48 @@ export default {
       type: String,
       default: '',
     },
+    color: {
+      type: String,
+      default: '',
+    },
   },
   data() {
     return {
       tempName: this.name,
       tempPassword: this.password,
+      tempColor: this.color,
     }
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.entrance-form {
+  .inputs {
+    height: 100%;
+    width: 100%;
+
+    display: grid;
+    grid-template-rows: 1fr 42px 42px 1fr;
+    grid-template-columns: 1fr 300px;
+    gap: 8px;
+
+    > * {
+      overflow: hidden;
+    }
+
+    .name {
+      grid-row: 2;
+      grid-column: 1;
+    }
+    .password {
+      grid-row: 3;
+      grid-column: 1;
+    }
+    .color {
+      grid-row: 1 / 5;
+      grid-column: 2;
+    }
+  }
+}
+</style>
