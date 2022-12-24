e27d95b26f5273bc85379eeeb9690a23277ce030
482F <482F.git.mail@482f.net>
2022-12-25 00:00:00 +0900

    データの同期を 1/60 秒に一度に制限

--- server-middleware/ws.js
+++ server-middleware/ws.js
@@ -1,4 +1,5 @@
 import WebSocket from 'ws'
+import throttle from 'lodash/throttle'
 
 const wss = new WebSocket.Server({
   port: 18245,
@@ -16,27 +17,51 @@ function send(client, key, value) {
   client.send(message)
 }
 
+const broadcast = throttle((room) => {
+  ;[...wss.clients].forEach((someClient) => {
+    send(someClient, 'changeValue', room.unBroadcasted[someClient.id])
+  })
+  Object.values(room.unBroadcasted).forEach((obj) => {
+    for (const key of Object.keys(obj)) {
+      delete obj[key]
+    }
+  })
+}, 1000 / 60)
+
 wss.on('connection', (client) => {
   // クライアントからのリクエストを受け取った際の処理
   client.on('message', (rawMessage) => {
-    // 送信元を含むすべてのWebSocket接続済みクライアントに送信
+    const room = rooms[client.password]
+
     const [, key, , message] = new TextDecoder()
       .decode(rawMessage)
       .match(/^([^-]+)(-(.+))?$/)
     const value = JSON.parse(message)
     if (key === 'changePublic') {
-      Object.assign(rooms[client.password], value)
-      ;[...wss.clients]
-        .filter((someClient) => someClient !== client)
-        .forEach((someClient) => {
-          send(someClient, 'changeValue', value)
-        })
-    } else if (key === 'password') {
-      const password = value
-      client.password = password
-      rooms[password] ??= {}
+      // changed は新規ログイン時に配信のために保存
+      Object.assign(room.changed, value)
 
-      send(client, 'changeValue', rooms[password])
+      // unBroadcasted は、自分ではない client へ配信するべきデータを保持する
+      // 自分が特定のキーを更新した場合、そのキーの値は自分に配信されなくて良いため、既存の値は削除する
+      Object.entries(room.unBroadcasted)
+        .filter(([id]) => client.id !== id)
+        .forEach(([, obj]) => Object.assign(obj, value))
+      for (const key of Object.keys(value)) {
+        delete room.unBroadcasted[client.id][key]
+      }
+
+      broadcast(room)
+    } else if (key === 'login') {
+      const { password, id } = value
+      client.password = password
+      client.id = id
+      rooms[password] ??= {
+        changed: {},
+        unBroadcasted: {},
+      }
+      rooms[password].unBroadcasted[id] ??= {}
+
+      send(client, 'changeValue', rooms[password].changed)
       send(client, 'initialized')
     }
   })
