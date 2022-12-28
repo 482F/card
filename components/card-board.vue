c6f146e2736ce4a5ec43556158d4ed15bdd73a60
482F <482F.git.mail@482f.net>
2022-12-29 00:09:39 +0900

    zIndex 周りの不具合を修正

--- components/card-board.vue
+++ components/card-board.vue
@@ -452,9 +452,12 @@ export default {
         this.cards
           .filter((someCard) => card.zIndex < someCard.zIndex)
           .forEach((card) => {
-            changeObj[`cards-${card.index}-zIndex`] = card.zIndex
+            changeObj[`cards-${card.index}-zIndex`] ??= card.zIndex
+            changeObj[`cards-${card.index}-zIndex`] -= 1
+            card.zIndex -= 1
           })
         changeObj[`cards-${card.index}-zIndex`] = this.cards.length
+        card.zIndex = this.cards.length
       })
       this.$emit('update', changeObj)
     },
