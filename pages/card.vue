09cc87ca2a6fba694318c1466958f1812060c4ba
482F <482F.git.mail@482f.net>
2022-12-29 23:25:52 +0900

    盤面のサイズを大きく

--- pages/card.vue
+++ pages/card.vue
@@ -11,15 +11,6 @@
     <div v-else class="main">
       <v-progress-circular v-if="!ready" indeterminate />
       <template v-else>
-        <card-board
-          class="card-board"
-          :mode="mode"
-          :cards="cards"
-          :angle="players[id]?.angle"
-          :players="players"
-          :id="id"
-          @update="changePublic($event)"
-        />
         <v-select
           class="mode-select"
           :items="modes"
@@ -28,14 +19,38 @@
           outlined
           dense
         />
-        <v-slider
-          class="angle-slider"
-          max="360"
-          min="0"
-          vertical
-          :value="players[id]?.angle"
-          @input="changePublic({ [`players-${id}-angle`]: $event })"
-        />
+        <div class="angle-slider-outer">
+          <v-slider
+            class="angle-slider"
+            max="360"
+            min="0"
+            vertical
+            :value="players[id]?.angle"
+            @input="changePublic({ [`players-${id}-angle`]: $event })"
+          />
+        </div>
+        <div
+          class="card-board-outer"
+          :style="{
+            '--outer-height':
+              (Math.abs(Math.sin(radian * 2)) * (Math.sqrt(2) - 1) + 1) *
+                boardSize *
+                1.2 +
+              'px',
+            '--board-diagonal-size': boardDiagonalSize + 'px',
+          }"
+        >
+          <card-board
+            class="card-board"
+            :board-size="boardSize"
+            :mode="mode"
+            :cards="cards"
+            :angle="players[id]?.angle"
+            :players="players"
+            :id="id"
+            @update="changePublic($event)"
+          />
+        </div>
       </template>
     </div>
   </div>
@@ -50,6 +65,7 @@ const modes = [
   { text: 'uno', value: 'uno' },
   { text: 'トランプ', value: 'trump' },
 ]
+const boardSize = 1200
 
 export default {
   name: 'Card',
@@ -59,6 +75,11 @@ export default {
   },
   computed: {
     modes: () => modes,
+    boardSize: () => boardSize,
+    boardDiagonalSize: () => Math.sqrt(2 * boardSize ** 2),
+    radian() {
+      return (this.players[this.id]?.angle * Math.PI) / 180
+    },
   },
   data() {
     const mode = modes[0].value
@@ -193,7 +214,7 @@ export default {
 
 <style lang="scss" scoped>
 .outer {
-  overflow: hidden;
+  // overflow: hidden;
   height: 100%;
   width: 100%;
   .main {
@@ -203,34 +224,48 @@ export default {
     display: grid;
     grid-template-rows: 50px 1fr;
     grid-template-columns: 40px 1fr;
-    > * {
-      overflow: hidden;
-    }
+    // > * {
+    //   overflow: hidden;
+    // }
 
     .mode-select {
+      z-index: 1;
+      position: sticky;
+      top: 30px;
       background-color: white;
       grid-row: 1;
       grid-column: 1 / 3;
     }
-    .angle-slider {
+    .angle-slider-outer {
       grid-row: 2;
       grid-column: 1;
+      .angle-slider {
+        position: sticky;
+        top: 110px;
 
-      > ::v-deep(.v-input__control) {
-        height: 99%;
-        > .v-input__slot {
-          height: 99%;
-          .v-slider {
-            height: 99%;
+        --height: 83vh;
+        > ::v-deep(.v-input__control) {
+          height: var(--height);
+          > .v-input__slot {
+            height: var(--height);
+            .v-slider {
+              height: var(--height);
+            }
           }
         }
       }
     }
-    .card-board {
+    .card-board-outer {
+      height: var(--outer-height);
+      margin-top: calc(-1 * var(--outer-height) * 0.08);
       justify-self: center;
       align-self: center;
       grid-row: 2;
       grid-column: 2;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
     }
   }
 }
